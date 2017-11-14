b8e0271c510b7265dd120a45c5e6bb274a8db121
Leif Hetland <lehetland1@gmail.com>
2017-11-14 14:33:45 -0600

    class, style, and event binding, also event filtering

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -10,14 +10,44 @@ import { CoursesService } from "./courses.service";
         {{course}}
       </li>
     </ul>
+    <h3> Bootstrap: </h3>
+    <button class="btn btn-primary" >Save</button>
+
+    <h3> Class Binding: </h3>
+    <button class="btn btn-primary" [class.active]="isActive">Save</button>
+
+    <h3> Style Binding: </h3>
+    <button class="btn btn-primary" [style.backgroundColor]="isActive ? 'blue' : 'white' ">Save</button>
+
+    <div (click)="onDivClicked()">
+      <h3> Event Binding & Bubbling: </h3>
+      <button class="btn btn-primary" (click)="onSave($event)"  >Save</button>
+    </div>
+
+    <h3> Event Filtering: </h3>
+    <input (keyup.enter)="onKeyUp()" />
   `
 })
 export class CoursesComponent {
   title = "List of courses";
   courses;
+  isActive = true;
+
+  onDivClicked(){
+    console.log("Div was clicked");
+  }
+
+  onSave($event){
+    $event.stopPropagation();
+    console.log("Button was clicked", $event);
+  }
+
+  onKeyUp() {
+    console.log("Enter was pressed")
+  }
 
   constructor(service: CoursesService) {
-    
+
     // let service = new CoursesService();
     this.courses = service.getCourses();
   }
