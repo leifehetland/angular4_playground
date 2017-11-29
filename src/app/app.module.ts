ceeb94e96b94459862b4a0055c45d3f9813afe26
Leif Hetland <lehetland1@gmail.com>
2017-11-29 15:50:19 -0600

    implemented zippy component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { TitleCasePipe } from './title-case.pipe';
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
-import { InputFormatDirective } from './input-format.directive'
+import { InputFormatDirective } from './input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component'
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { InputFormatDirective } from './input-format.directive'
     TitleCasePipe,
     PanelComponent,
     LikeComponent,
-    InputFormatDirective
+    InputFormatDirective,
+    ZippyComponent
   ],
   imports: [
     BrowserModule,
