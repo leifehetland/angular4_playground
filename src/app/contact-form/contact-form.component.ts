dff5e36058ddc6e07a0c8e0d1bba22d530a17f77
Leif Hetland <lehetland1@gmail.com>
2018-02-03 15:36:57 -0600

    built a template driven form

---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent {
+  categories = [
+    {id: 1, name: 'Development'},
+    {id: 2, name: 'Art'},
+    {id: 3, name: 'Languages'}
+  ];
+
+  log(x) {
+    console.log(x);
+  }
+
+  submit(f) {
+    console.log(f);
+  }
+}
