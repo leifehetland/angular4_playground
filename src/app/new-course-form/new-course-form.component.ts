b9c5e53922886b2aee19622c30dac5d7005e82ef
Leif Hetland <lehetland1@gmail.com>
2018-02-04 16:22:52 -0600

    edits to get forms working properly

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -33,18 +33,18 @@ export class NewCourseFormComponent implements OnInit {
   });
 
 
-  // addTopic(topic: HTMLInputElement) {
-  //   this.topics.push(new FormControl(topic.value));
-  //   topic.value = '';
-  // }
-  //
-  // removeTopic(topic: FormControl) {
-  //   let index = this.topics.controls.indexOf(topic);
-  //   this.topics.removeAt(index);
-  // }
-  //
-  // get topics() {
-  //   return this.form.get('topics') as FormArray;
-  // }
+  addTopic(topic: HTMLInputElement) {
+    this.topics.push(new FormControl(topic.value));
+    topic.value = '';
+  }
+
+  removeTopic(topic: FormControl) {
+    let index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+
+  get topics() {
+    return this.form.get('topics') as FormArray;
+  }
 
 }
