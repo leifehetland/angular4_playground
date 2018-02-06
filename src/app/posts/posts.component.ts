60a5ce184233a926a679e033f2ec76c17e072ace
Leif Hetland <lehetland1@gmail.com>
2018-02-05 19:03:13 -0600

    handling expected errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -17,6 +17,9 @@ export class PostsComponent implements OnInit {
       .subscribe(response => {
         console.log("Response: ", response.json());
         this.posts = response.json();
+      }, error => {
+        alert('An unexpected error occured.');
+        console.log(error);
       });
   }
 
@@ -30,6 +33,13 @@ export class PostsComponent implements OnInit {
         post.id = response.json().id;
         this.posts.splice(0, 0, post)
         console.log("Response: ", response.json());
+      }, (error: Response) => {
+        if (error.status === 400) {
+          // this.form.setErrors(error.json());
+        } else {
+          alert('An unexpected error occured.');
+          console.log(error);
+        }
       });
   }
 
@@ -37,16 +47,26 @@ export class PostsComponent implements OnInit {
     this.service.updatePosts(post)
       .subscribe(response => {
         console.log('RESPONSE', response);
-      })
+      }, error => {
+        alert('An unexpected error occured.');
+        console.log(error);
+      });
     // this.http.put(this.url, JSON.stringify(post))
   }
 
   deletePost(post) {
-    this.service.deletePost(post.id)
+    this.service.deletePost(345)
       .subscribe(response => {
         let index = this.posts.indexOf(post);
         this.posts.splice(index, 1);
-      })
+      }, (error: Response) => {
+        if (error.status === 404) {
+          alert('This post has already been deleted.');
+        } else {
+          alert('An unexpected error occured.');
+          console.log(error);
+        }
+      });
   }
 
 }
